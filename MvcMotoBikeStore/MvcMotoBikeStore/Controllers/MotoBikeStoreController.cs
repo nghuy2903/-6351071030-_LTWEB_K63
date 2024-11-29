5040c64c67ddbaa8868b927152cedb10162d7434
Nguyen Huy <nghuy29032004@gmail.com>
2024-11-29 15:12:53 +0700

    complete LAB_07: pagination

--- MvcMotoBikeStore/MvcMotoBikeStore/Controllers/MotoBikeStoreController.cs
+++ MvcMotoBikeStore/MvcMotoBikeStore/Controllers/MotoBikeStoreController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using MvcMotoBikeStore.Models;
+using PagedList;
 
 namespace MvcMotoBikeStore.Controllers
 {
@@ -17,20 +19,30 @@ namespace MvcMotoBikeStore.Controllers
         }
 
         // GET: MotoBikeStore
-        private List<XEGANMAY> Layxemoi(int count)
+        private List<XEGANMAY> Layxemoi(int skip, int count)
         {
-            return db.XEGANMAYs.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+            return db.XEGANMAYs.OrderByDescending(a => a.Ngaycapnhat).Skip(skip).Take(count).ToList();
         }
 
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            var xemoi = Layxemoi(5);
-            return View(xemoi);
+            int pageSize = 3; 
+            int pageNumber = page ?? 1;
+            int skip = (pageNumber - 1) * pageSize;
+
+            var xeMoi = Layxemoi(skip, pageSize);
+            var pagedXeMoi = new StaticPagedList<XEGANMAY>(xeMoi, pageNumber, pageSize, db.XEGANMAYs.Count());
+
+            return View(pagedXeMoi);
         }
 
         public ActionResult Mota() 
         {
+            
+
             var mota = db.HANGSANXUATs.ToList();
+
+            //var pagedMota = mota.ToPagedList(pageNumber, pageSize);
 
             return PartialView(mota);
         }
@@ -41,8 +53,12 @@ namespace MvcMotoBikeStore.Controllers
             return PartialView(loaixe);
         }
 
-        public ActionResult SPTheoChuDe(int id)
+        public ActionResult SPTheoChuDe(int id, int? page)
         {
+            ViewBag.CurrentCategoryId = id;
+            int pageSize = 3;
+            int pageNumber = page ?? 1;
+
             var products = from xe in db.XEGANMAYs
                            join sx in db.SANXUATXEs on xe.MaXe equals sx.MaXe
                            join hsx in db.HANGSANXUATs on sx.MaHSX equals hsx.MaHSX
@@ -54,11 +70,20 @@ namespace MvcMotoBikeStore.Controllers
                                SanXuatXe = sx
                            };
 
-            return View(products.ToList());
+            var sortedProducts = products.OrderBy(p => p.XeGanMay.TenXe);  // Example sorting by product name
+
+            // Paginate the result
+            var pagedProducts = sortedProducts.ToPagedList(pageNumber, pageSize);
+
+            return View(pagedProducts);
         }
 
-        public ActionResult SPTheoLoaiXe(int id)
+        public ActionResult SPTheoLoaiXe(int id, int? page)
         {
+            ViewBag.CurrentCategoryId = id;
+            int pageSize = 3;
+            int pageNumber = page ?? 1;
+
             var products = from xe in db.XEGANMAYs
                            join lx in db.LOAIXEs on xe.MaLX equals lx.MaLX
                            where lx.MaLX == id
@@ -68,7 +93,12 @@ namespace MvcMotoBikeStore.Controllers
                                LoaiXe = lx
                            };
 
-            return View(products.ToList());
+            var sortedProducts = products.OrderBy(p => p.XeGanMay.TenXe);  
+
+            
+            var pagedProducts = sortedProducts.ToPagedList(pageNumber, pageSize);
+
+            return View(pagedProducts);
         }
 
         public ActionResult Details(int id)
